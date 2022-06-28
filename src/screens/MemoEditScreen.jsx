d928329c83153b84e36073ab5d87500c3fdc25b0
cocomacha <97824171+cocomacha@users.noreply.github.com>
2022-06-28 22:58:08 +0900

    Create LogInScreen and SignUpScreen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -7,7 +7,7 @@ import KeyboardSafeView from "../components/KeyboardSafeView";
 
 export default function MemoEditScreen() {
   return (
-    <KeyboardSafeView style={StyleSheet.container}>
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
@@ -24,11 +24,12 @@ const styles = StyleSheet.create({
   inputContainer: {
     paddingHorizontal: 27,
     paddingVertical: 32,
+    flex: 1,
   },
   input: {
-    backgroundColor: "blue",
     textAlignVertical: "top",
     fontSize: 16,
     lineHeight: 24,
+    flex: 1,
   },
 });
